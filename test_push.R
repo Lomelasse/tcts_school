b8e186d40ae34179c2ae76eac310cc61cb7a50b3
Lomelasse <lomelasse@gmail.com>
2017-10-07 13:07:53 +0400

    first commit

---
+++ test_push.R
@@ -0,0 +1,19 @@
+set.seed(123)
+rnorm(10)
+# r по умолчанию использует банковское округление (т.е округление к ближайшему четному)
+
+#90 subj, factors: 0/1, a/b/c, value rnorm+runif with 15% NA for test m = 3, for control group m = 9
+id <- 1:90
+test <- c(rep(0, 45), rep(1, 45))
+colour <- rep(c("a", "b","c"), 30)
+value <- c((rnorm(45, 3) + runif(45)), (rnorm(45,9) + runif(45)))
+index_NA <- sample(1:90, round(90*0.15))
+value[index_NA] <- NA
+# a <- runif(1)
+# b1 <- runif(1)
+# b2 <- runif(1)
+# b3 <- runif(1)
+# y <- a + b1*factor1+b2*factor2+b3*factor1*factor2    
+
+data <-data.frame(id, test, colour, value)
+t.test(value ~ test, data = data[data$colour == 'c',])
